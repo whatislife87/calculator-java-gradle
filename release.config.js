72f2e61f342c548355c6fe0c5714a6588eb2da54
Esteban Pulgarin Arango <esteban-pulgarin.a@gmail.com>
2024-05-25 03:10:50 -0500

    refactor: cambio refactor

--- release.config.js
+++ release.config.js
@@ -2,6 +2,21 @@ const config = {
   branches: ['main'],
   plugins: [
     '@semantic-release/commit-analyzer',
+    {
+      "preset": "angular",
+      "releaseRules": [
+        {"type": "docs", "release": "patch"},
+        {"type": "style", "release": "patch"},
+        {"type": "refactor", "release": "patch"},
+        {"type": "perf", "release": "patch"},
+        {"type": "test", "release": "patch"},
+        {"type": "build", "release": "patch"},
+        {"type": "ci", "release": "patch"},
+        {"type": "fix", "release": "patch"},
+        {"type": "feat", "release": "minor"},
+        {"type": "major", "release": "major"}  // Nueva etiqueta para major
+      ]
+    },
     '@semantic-release/release-notes-generator',
     ["@semantic-release/git", {
       "assets": ["dist/*.js", "dist/*.js.map"],
